ca77d9cc5bac6c4741d50fd4997b002115cafd03
Mohamed Salman <mohamed.akj@gmail.com>
2023-07-16 18:15:55 +0530

    Removed cross-origin logics from main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import notes
 from app import models
@@ -8,18 +7,6 @@ from .database import engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(notes.router, prefix="/api/notes", tags=["notes"])
 
